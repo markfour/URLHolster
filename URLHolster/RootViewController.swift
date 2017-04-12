4ade1917522328285fc98db7f297e31a49612d28
markfour <axhfaro@gmail.com>
2017-04-12 14:35:28 +0900

    Initialize

---
+++ URLHolster/RootViewController.swift
@@ -0,0 +1,57 @@
+//
+//  RootViewController.swift
+//  URLHolster
+//
+//  Created by kazumi hayashida on 2017/02/12.
+//  Copyright © 2017年 kazumi hayashida. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+  
+  
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  fileprivate var URLItems: [URLItem] = []
+  
+  override func viewDidLoad() {
+    
+    let i1 = URLItem(title: "google", URL: URL(string: "https://www.google.co.jp/")!)
+    URLItems.append(i1)
+    let i2 = URLItem(title: "apple", URL: URL(string: "https://www.apple.com/jp")!)
+    URLItems.append(i2)
+    
+    
+  }
+  
+  func getURL() {
+    
+  }
+}
+
+extension RootViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return URLItems.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "")
+    let item = URLItems[indexPath.row]
+    
+    cell.textLabel?.text = item.title
+    cell.detailTextLabel?.text = item.URL.absoluteString
+    
+    return cell
+  }
+}
+
+extension RootViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    let item = URLItems[indexPath.row]
+    
+    UIApplication.shared.open(item.URL, options: [:], completionHandler: nil)
+  }
+}
---
+++ URLHolster/URLItem.swift
@@ -0,0 +1,21 @@
+//
+//  URLItem.swift
+//  URLHolster
+//
+//  Created by kazumi hayashida on 2017/02/12.
+//  Copyright © 2017年 kazumi hayashida. All rights reserved.
+//
+
+import Foundation
+
+class URLItem {
+  var title: String
+  var URL: URL
+  
+  init(title: String, URL: URL) {
+    self.title = title
+    self.URL = URL
+  }
+}
+
+
