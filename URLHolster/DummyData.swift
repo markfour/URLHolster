038541846a7db6ee228c23b80e797c3d382b23b8
markfour <axhfaro@gmail.com>
2018-02-01 19:23:19 +0900

    Implement enableDummy()

--- URLHolster/DummyData.swift
+++ URLHolster/DummyData.swift
@@ -5,4 +5,14 @@
 //  Copyright © 2018 kazumi hayashida. All rights reserved.
 //
 
+import Foundation
 
+class DummyData {
+  class func enableDummy() -> Bool {
+    if let enableDummy = ProcessInfo.processInfo.environment["dummy_data"] {
+      return enableDummy == "1"
+    } else {
+      return false
+    }
+  }
+}
