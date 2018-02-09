f31ebe6d42dbe4a30e42d14c66a914a0d5861e8a
markfour <axhfaro@gmail.com>
2018-02-09 18:07:59 +0900

    DummyData を実装

--- URLHolster/DummyData.swift
+++ URLHolster/DummyData.swift
@@ -8,11 +8,46 @@
 import Foundation
 
 class DummyData {
-  class func enableDummy() -> Bool {
+  static let userDefaults = UserDefaults.standard
+  
+  class func enable() -> Bool {
     if let enableDummy = ProcessInfo.processInfo.environment["dummy_data"] {
       return enableDummy == "1"
     } else {
       return false
     }
+  }
+  
+  class func didIntialize() -> Bool {
+    return userDefaults.bool(forKey: "didInitialize")
+  }
+
+  class func initializeData() {
+    guard let path = Bundle.main.path(forResource: "URLItemDummyData.plist", ofType:nil) else {
+      print("error")
+      assertionFailure()
+      return
+    }
+    guard let data = NSArray(contentsOfFile: path) else {
+      print("error")
+      assertionFailure()
+      return
+    }
+    userDefaults.set(data, forKey: "dummyData")
+    userDefaults.set(true, forKey: "didInitialize")
+  }
+  
+  class func fetch() -> [URLItem] {
+    if !didIntialize() {
+      initializeData()
+    }
+    var urlItems: [URLItem] = []
+    
+    let dummyDataArray = userDefaults.array(forKey: "dummyData")!
+    dummyDataArray.forEach {
+       urlItems.append(URLItem(dict: $0 as! [String: Any]))
+    }
+    
+    return urlItems
   }
 }
