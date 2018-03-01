d6983a0eccc2e364e9b0ccf4fe323bd80fe64729
markfour <axhfaro@gmail.com>
2018-03-02 00:45:57 +0900

    ItemTableViewCell.swift を追加

---
+++ URLHolster/ItemTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ItemTableViewCell.swift
+//  URLHolster
+//
+//  Created by kazumi hayashida on 2018/03/02.
+//  Copyright © 2018年 kazumi hayashida. All rights reserved.
+//
+
+import Foundation
